8bcec8d0dea79c0987e5150308a5d1d638a1c8b1
Vladimir Chebukin <vovachebr@mail.ru>
2024-05-16 22:16:44 +0300

    Добавление restRequestTimeout в Discord

--- server/index.js
+++ server/index.js
@@ -24,14 +24,6 @@ job.start();
 
 discordBot.login(DISCORD_BOT_TOKEN);
 const app = express();
-app.use((req, res, next) => {
-  if(req.url.startsWith('/api/images/getImageByName')) {
-    next();
-  }
-}), basicAuth({
-  authorizer: (username, password) => !!(username && password),
-  unauthorizedResponse: () => 'No credentials provided'
-});
 app.use(morgan('tiny'));
 
 app.use(bodyParser.json());
